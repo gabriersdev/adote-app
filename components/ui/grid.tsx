d8257aaaed1acb945b287ad8766fc763a0c185fb
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-05-02 16:39:45 -0300

    feat: componente grid

---
+++ components/ui/grid.tsx
@@ -0,0 +1,21 @@
+import {ReactNode, JSX} from "react";
+
+type GridProps = {
+  children: ReactNode;
+  cols?: number;
+  gap?: number;
+  className?: string;
+};
+
+const Grid = ({children, cols = 1, gap = 4, className = ""}: GridProps): JSX.Element => {
+  const gridCols = `grid-cols-${cols}`;
+  const gridGap = `gap-${gap}`;
+
+  return (
+    <div className={`grid ${gridCols} ${gridGap} ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default Grid;
