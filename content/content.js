9b05117f5700c89051e1d675cef931e612299ebc
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-05-26 11:32:33 -0300

    feat: class content.js

---
+++ content/content.js
@@ -0,0 +1,22 @@
+export default class Content {
+  static UI() {
+    return {
+      "app-name": "Adote",
+    }
+  }
+  
+  static Abrigo () {
+    return {
+      "name": "Abrigo",
+      "description": "A description",
+    }
+  }
+  
+  static Error404 () {
+    return {
+      "name": "#404",
+      "description": "Página não encontrada",
+      "message": "A página que você tentou acessar não existe. Retorne para a página inicial."
+    }
+  }
+}
